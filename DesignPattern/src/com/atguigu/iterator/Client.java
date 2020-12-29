785f83237f0ffb9588ac7240942ee694d27d4fc2
高老头2458 <52886289@qq.com>
2020-12-29 08:39:56 +0800

    迭代器模式

--- DesignPattern/src/com/atguigu/iterator/Client.java
+++ DesignPattern/src/com/atguigu/iterator/Client.java
@@ -6,18 +6,16 @@ import java.util.List;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//创建学院
-		List<College> collegeList = new ArrayList<College>();
-		
+		List<College> collegeList = new ArrayList<>();
+
 		ComputerCollege computerCollege = new ComputerCollege();
 		InfoCollege infoCollege = new InfoCollege();
-		
+
 		collegeList.add(computerCollege);
-		//collegeList.add(infoCollege);
-		
-		OutPutImpl outPutImpl = new OutPutImpl(collegeList);
-		outPutImpl.printCollege();
+		collegeList.add(infoCollege);
+
+		OutPutImpl outPut = new OutPutImpl(collegeList);
+		outPut.printCollege();
 	}
 
 }
